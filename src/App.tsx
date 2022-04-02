1796b1a3fca808fc03403b1d76f523a8d0120366
kitamuraDev <masato1999824@icloud.com>
2022-04-02 13:57:59 +0900

    :sparkles: フォーム実装完了

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import './App.css';
 
 import { VFC } from 'react';
 
-const App: VFC = () => <div className='App'>react</div>;
+const App: VFC = () => (
+  <div className='App'>
+    <div>
+      <h2 className='title'>Todo App Powered by React TypeScript</h2>
+      {/* eslint-disable */}
+      <form onSubmit={() => {}}>
+        <input type='text' onChange={() => {}} className='inputText' />
+        <input type='submit' value='追加' className='submitButton' />
+      </form>
+    </div>
+  </div>
+);
 
 export default App;
