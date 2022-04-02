dd4d4c7b72d49b8dc241539a61ab1a7b4078f877
kitamuraDev <masato1999824@icloud.com>
2022-04-02 14:56:51 +0900

    :sparkles: インプットに関する実装。todo型の追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
 import './App.css';
 
-import { VFC } from 'react';
+import { useState, VFC } from 'react';
+import { Todo } from 'types/todoType';
 
-const App: VFC = () => (
-  <div className='App'>
-    <div>
-      <h2 className='title'>Todo App Powered by React TypeScript</h2>
-      {/* eslint-disable */}
-      <form onSubmit={() => {}}>
-        <input type='text' onChange={() => {}} className='inputText' />
-        <input type='submit' value='追加' className='submitButton' />
-      </form>
+const App: VFC = () => {
+  const [todo, setTodo] = useState<Todo[]>([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleChangeInputValue = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div className='App'>
+      <div>
+        <h2 className='title'>Todo App Powered by React TypeScript</h2>
+        {/* eslint-disable */}
+        <form onSubmit={() => {}}>
+          <input
+            type='text'
+            onChange={handleChangeInputValue}
+            value={inputValue}
+            className='inputText'
+          />
+          <input type='submit' value='追加' className='submitButton' />
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
