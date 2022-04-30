59d6376cd270c8affb0129c9e7015b7d4d702389
kitamuraDev <masato1999824@icloud.com>
2022-04-30 16:33:17 +0900

    :recycle: カスタムフックにインターフェースを定義して戻り値の見通しを改善 #25

--- src/types/todo/index.ts
+++ src/types/todo/index.ts
@@ -3,3 +3,10 @@ export type Todo = {
   value: string;
   isDone: boolean;
 };
+
+export type Handler = {
+  handleAddTodo: (e: React.FormEvent<HTMLFormElement>) => void;
+  handleEditTodoValue: (id: Todo['id'], editTodo: Todo['value']) => void;
+  handleToggleIsDone: (id: Todo['id'], isDone: Todo['isDone']) => void;
+  handleDeleteTodo: (id: Todo['id']) => void;
+};
